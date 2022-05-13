0b220b5f2def7c2e29d676dbc11e3e69bebfbbd0
VladimirSizov <vladimir.s.sizov@gmail.com>
2022-05-13 22:35:05 +0300

    2022.05.13_22:34

--- ayumu/account/views.py
+++ ayumu/account/views.py
@@ -13,7 +13,7 @@ def user_login(request):
 			cd = form.cleaned_data
 			user = authenticate(request, username=cd['username'], password=cd['password'])
 			if user is not None:
-				if user.is_acive:
+				if user.is_active:
 					login(request, user)
 					return HttpResponse('Authenticated successfully')
 				else:
